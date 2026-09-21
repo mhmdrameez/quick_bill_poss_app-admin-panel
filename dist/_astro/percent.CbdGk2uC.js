55112440ae2d385778dbe4ec7b98912a44f3849d
mhmdrameez <muhammedrameez2000@gmail.com>
2026-09-21 12:52:28 +0530

    chore: add Astro dev server metadata file

--- dist/_astro/percent.CbdGk2uC.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./loader-circle.CCZqADf6.js";var t={name:`percent`,size:24,node:[[`line`,{x1:`19`,x2:`5`,y1:`5`,y2:`19`,key:`1x9vlm`}],[`circle`,{cx:`6.5`,cy:`6.5`,r:`2.5`,key:`4mh3h7`}],[`circle`,{cx:`17.5`,cy:`17.5`,r:`2.5`,key:`1mdrzq`}]]};t.node;var n=e(t);export{n as t};
